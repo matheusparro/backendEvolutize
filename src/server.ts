d6f862409cf39b1e81598914c3363ba0fdb9ede0
Matheus Parro de Sousa <matheus.parro@evolutize.com.br>
2024-04-16 17:13:52 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,59 @@
+import express from 'express';
+import cors from 'cors';
+import WebPush from 'web-push';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+// WebPush
+
+const publicKey = 'BGL4bOMi4aRjGoBCyqL1aTzLUcqlVw_iPfuMdpivuYJsVkOzxA4hHoVTeIZ6nZpMvw2j84B985TS6h9LVsMfoVc';
+const privateKey = '_Muz8v4Ht9hCIBUgGncblTusRiJXUeLDx0U4Kw4Do94';
+
+WebPush.setVapidDetails('https://localhost:3333', publicKey, privateKey);
+
+
+// Routes
+
+interface ISubscription {
+  subscription: {
+    endpoint: string;
+    keys: {
+      p256dh: string;
+      auth: string;
+    };
+  };
+}
+
+app.get('/notification/push/public_key', (request, response) => {
+  return response.json({ publicKey });
+});
+app.get('/', (request, response) => {
+  return response.send("TESTE");
+});
+
+app.post('/notification/push/register', (request, response) => {
+  console.log(request.body);
+
+  return response.sendStatus(201);
+});
+
+app.post('/notification/push/send', async (request, response) => {
+  const { subscription } = request.body as ISubscription;
+
+  WebPush.sendNotification(
+    subscription,
+    JSON.stringify({
+      icon: 'your-icon-link.png',
+      title: 'NOTIFICAÇÃO EVOLUTIZE',
+      body: 'Content of your message',
+      imageUrl: 'your-image-link.png'
+    }),
+  );
+
+  return response.sendStatus(201);
+});
+
+app.listen(3333, () => console.log('SERVER IS RUNNING AT :3333'));
